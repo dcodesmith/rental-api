a6a087d2b54a47f79fe728405fc9d8f6378974c6
Afees Kolawole <dcodesmith@gmail.com>
2024-01-27 23:09:09 +0000

    set up authentication

---
+++ src/iam/iam.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { AuthenticationController } from './authentication/authentication.controller';
+import { AuthenticationService } from './authentication/authentication.service';
+import { AuthenticationGuard } from './authentication/guards/authentication/authentication.guard';
+import jwtConfig from './config/jwt.config';
+import { BcryptService } from './hashing/bcrypt.service';
+import { HashingService } from './hashing/hashing.service';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
+import { RefreshTokenIdsStorage } from './authentication/refresh-token-ids.storage';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    ConfigModule.forFeature(jwtConfig),
+  ],
+  providers: [
+    {
+      provide: HashingService,
+      useClass: BcryptService,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+    RefreshTokenIdsStorage,
+    AccessTokenGuard,
+    AuthenticationService,
+  ],
+  controllers: [AuthenticationController],
+})
+export class IamModule {}
