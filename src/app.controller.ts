6c18f3f629bf4cdda3de231eb62786dcc58172fd
Afees Kolawole <dcodesmith@gmail.com>
2024-01-28 17:07:19 +0000

    make base route public

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthType } from './iam/authentication/enums/auth-type-enum';
+import { Auth } from './iam/authentication/decorators/auth.decorator';
 
+@Auth(AuthType.None)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
